21da27872881f6d7c8bbba19f0b81b8d4694e070
Benjamin Jordan <benjamin@thegoldenmule.com>
2024-04-11 12:54:35 -0500

    threejs render loop

--- code/src/solution.js
+++ code/src/solution.js
@@ -1,19 +1,28 @@
 import * as THREE from 'three';
 
-window.init = async (canvas) => {
-  const scene = new THREE.Scene();
+let renderer, scene, camera;
+
+window.init = async () => {
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera.position.z = 5;
+
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  
 
   console.log('made a scene', scene);
 };
 
 let speed = [0, 0, 0];
-window.loop = (dt, canvas, input) => {
-  /** example
-  if (input.keys.has('ArrowUp')) {
-    speed[1] += 0.1;
-  } else {
-    speed[1] *= 0.9;
-  }
-  */
+window.loop = (dt, input) => {
+  renderer.render( scene, camera );
 };
 
